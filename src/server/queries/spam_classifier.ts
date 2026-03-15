003c965134974c13a370a8cd93ad457ae59c07d7
AlanNin <alanbusinessnin@gmail.com>
2026-03-15 18:03:17 -0400

    chore: deleted the bs, fixed api endpoint

--- src/server/queries/spam_classifier.ts
+++ src/server/queries/spam_classifier.ts
@@ -1,11 +1,11 @@
 "use server";
 import axios from "axios";
+import { env } from "~/env";
 
 export async function ClassifyEmail(email: string) {
   try {
-    const result = (
-      await axios.post("https://spammy-api.onrender.com/classify", { email })
-    ).data;
+    const result = (await axios.post(`${env.API_URL}/classify`, { email }))
+      .data;
 
     return result;
   } catch (error) {
